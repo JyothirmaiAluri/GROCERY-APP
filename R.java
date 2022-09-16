78316881b9e28c7a3d1ce9b9326130bbcb224065
JyothirmaiAluri <113624739+JyothirmaiAluri@users.noreply.github.com>
2022-09-16 19:20:58 +0530

    Add files via upload

--- R.java
+++ R.java
@@ -4,7 +4,7 @@
  * gradle plugin from the resource data it found. It
  * should not be modified by hand.
  */
-package androidx.slidingpanelayout;
+package androidx.swiperefreshlayout;
 
 public final class R {
     private R() {}
